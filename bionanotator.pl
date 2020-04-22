a0cb50a632bbf9c3c4aa3783d2e796e6aba1765d
daaaaande <worldtour@live.de>
2020-04-22 11:56:37 +0200

    now self deciding if allele frequencies will be discarded or not

--- bionanotator.pl
+++ bionanotator.pl
@@ -68,12 +68,14 @@ my$annovar_ex=`perl ../../table_annovar.pl  ../$infile ../../humanhg38/ -buildve
 print ER "executed annovar: $annovar_ex\n";
 
 
-# delete all non-csv files in run dir
+# delete all non-txt files in run dir
 if($delete_in_between_files){
   my$del_between_files=`find . -type f -not -name '*.txt' -print0 | xargs -0 rm --`;
   print ER "deleted all in-between files per users request: $del_between_files\n";
 }
-
+else{
+  print ER "set to not delete annovar in-between files.\n";
+}
 
 my$outfile_found=`ls -1f *.txt`;
 chomp $outfile_found;
@@ -85,15 +87,21 @@ open(FIN,">$sample.annotated.tsv") || die "$!\ncould not write the final outfile
 open(OU,$outfile_found)|| die "$!\ncould not open annovar outfile in run_$sample\n";
 
 my@all_out_lines=<OU>;
-
+# TODO: remove AF columns only if all empty, else let them stay
 my$line_count=0;
+my@file_lines=();
+my$full_header_line=0;
+my@lines_with_afs=();# full line with AF values
+# two possible header lines: with and without allele freqs
 my$new_header_line="Chr	Start	End	Ref	Alt	Func.refGene\tGene.refGene\tCancer.hallmark	GeneDetail.refGene	cytoBand	dgvMerged	gwasCatalog	wgEncodeRegDnaseClustered	genomicSuperDups	wgRna\n";
-print FIN "$new_header_line"; # print the header into final output file
+my$w_af_header="Chr	Start	End	Ref	Alt	Func.refGene\tGene.refGene\tCancer.hallmark	GeneDetail.refGene	cytoBand	dgvMerged	gwasCatalog	wgEncodeRegDnaseClustered	genomicSuperDups	wgRna\tAF	AF_raw	AF_male	AF_female	AF_afr	AF_ami	AF_amr	AF_asj	AF_eas	AF_fin	AF_nfe	AF_oth	AF_sas\n";
+
 my$hallm="";
+my$allele_freqs=""; #- attach here all allelle frequencies of the whole file- if there is more than . in there, leave it in the file
 foreach my $outline(@all_out_lines){
-  $line_count=$line_count +1;
+  $line_count=$line_count + 1;
   chomp $outline;
-  if($line_count == 1){# header
+  if($line_count == 1 ){# header
   # save header, edit as needed
 
   }
@@ -108,8 +116,8 @@ foreach my $outline(@all_out_lines){
       $single_gene=~s/\"//g;
       if(grep(/$single_gene?/,@allehallmarkg)){
         if($mapping_hash{$single_gene}=~/[A-Z]/){
-        my$hallm_neu=$mapping_hash{$single_gene};# one hallmark max per SV for now
-        $hallm="$hallm"."$hallm_neu";# collecting all hallmarks
+          my$hallm_neu=$mapping_hash{$single_gene};# one hallmark max per SV for now
+          $hallm="$hallm"."+"."$hallm_neu";# collecting all hallmarks
 
         }
       }
@@ -119,15 +127,52 @@ foreach my $outline(@all_out_lines){
     if(!($hallm=~/[A-z]/)){
       $hallm="none";
     }
+
+
+    $allele_freqs="$allele_freqs"."$all_line_parts[16]";
     # all other file lines
     # split by column
+    # cleanup the alleles freqs from . and whitespace?
+
+    if($allele_freqs=~/[0-9]/g){
+      # leave the allele frequencies in the output file
+      # change the header line
+      $full_header_line=1;
+    }
+
+
     my$new_line="$all_line_parts[0]\t$all_line_parts[1]\t$all_line_parts[2]\t$all_line_parts[3]\t$all_line_parts[4]\t$all_line_parts[5]\t$all_line_parts[6]\t$hallm\t$all_line_parts[7]\t$all_line_parts[10]\t$all_line_parts[11]\t$all_line_parts[12]\t$all_line_parts[13]\t$all_line_parts[14]\t$all_line_parts[15]\n";
     # cleanup
     $new_line=~s/\t\s+/\t/g;
-    print FIN $new_line;
+    #print FIN $new_line;
+    push(@file_lines,$new_line);
 
+    my$new_line_with_AF="$all_line_parts[0]\t$all_line_parts[1]\t$all_line_parts[2]\t$all_line_parts[3]\t$all_line_parts[4]\t$all_line_parts[5]\t$all_line_parts[6]\t$hallm\t$all_line_parts[7]\t$all_line_parts[10]\t$all_line_parts[11]\t$all_line_parts[12]\t$all_line_parts[13]\t$all_line_parts[14]\t$all_line_parts[15]\t$all_line_parts[16]\t$all_line_parts[17]\t$all_line_parts[18]\t$all_line_parts[19]\t$all_line_parts[20]\t$all_line_parts[21]\t$all_line_parts[22]\t$all_line_parts[23]\t$all_line_parts[24]\t$all_line_parts[25]\t$all_line_parts[26]\t$all_line_parts[27]\t$all_line_parts[28]\n";
+
+    $new_line_with_AF=~s/\t\s+/\t/g;
+    push(@lines_with_afs,$new_line_with_AF);
   }
 }
-#
+
+# now print the final file- first decide on the header
+
+if($full_header_line){
+  print FIN $w_af_header;
+  print ER "found allele frequencies, thus including them ...\n";
+  foreach my $line_w_af(@lines_with_afs){
+    print FIN $line_w_af;
+  }
+
+}
+elsif($full_header_line==0){
+  print FIN $new_header_line;
+  print ER "found  NO allele frequencies, thus excluding them ...\n";
+  foreach my $raw_line (@file_lines){
+    print FIN $raw_line;
+  }
+}
+
+
+
 print "done creating $sample.annotated.tsv.\n";
 1;
